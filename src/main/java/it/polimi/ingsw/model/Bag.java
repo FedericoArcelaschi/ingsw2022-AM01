614900dcfa6b107f308e386022667938dce1dba0
LorenzoAicardi <lorenzo.aicardi@mail.polimi.it>
2022-03-23 15:24:24 +0100

    Old bag

--- src/main/java/it/polimi/ingsw/model/Bag.java
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -1,6 +1,4 @@
 package it.polimi.ingsw.model;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import java.util.Arrays;
 import java.util.Random;
 
@@ -13,52 +11,48 @@ public class Bag {
         return seed;
     }
 
-    public Bag(){
+    public Bag() {
         students = new int[]{24, 24, 24, 24, 24};
         random = new Random();
     }
 
-    public Bag(long seed){
+    public Bag(long seed) {
         this.seed = seed;
         students = new int[]{24, 24, 24, 24, 24};
         random = new Random(seed);
     }
 
-    public Color extract(){
+    public Color extract() {
         int r;
         int color = 0;
         int rs = remainingStudents();
-        if(rs <=0) return null;
+        if (rs <= 0) return null;
         r = random.nextInt(rs);
-        for(int studentsForColor : students){
-            if(r>studentsForColor){
-                r-=studentsForColor;
+        for (int studentsForColor : students) {
+            if (r > studentsForColor) {
+                r -= studentsForColor;
                 color++;
             }
         }
         students[color]--;
-        switch(color){
-            case 0: return Color.YELLOW;
-            case 1: return Color.GREEN;
-            case 2: return Color.PINK;
-            case 3: return Color.BLUE;
-            case 4: return Color.RED;
+        switch (color) {
+            case 0:
+                return Color.YELLOW;
+            case 1:
+                return Color.GREEN;
+            case 2:
+                return Color.PINK;
+            case 3:
+                return Color.BLUE;
+            case 4:
+                return Color.RED;
         }
         return null;
     }
 
-    private int remainingStudents(){
+    private int remainingStudents() {
         return Arrays.stream(students).sum();
     }
-}
-=======
-=======
->>>>>>> Stashed changes
 
-public class Bag {
-    public
 }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
