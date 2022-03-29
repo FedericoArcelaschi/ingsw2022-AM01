f0cc3c519c95533cad6984f2453dd05d84e8111d
FedericoArcelaschi-Polimi <10654781@polimi.it>
2022-03-29 09:30:10 +0200

    Added WRSize to handle games with 2-4 or 3 players

--- src/main/java/it/polimi/ingsw/model/Castle.java
+++ src/main/java/it/polimi/ingsw/model/Castle.java
@@ -9,11 +9,11 @@ public class Castle {
     private Card lastCardPlayed;
     private final Team towerColor;
     private final int WRSize;
- 
-    public Castle(String PLayerID, Team team, int nPlayer, List<Color> c){
+
+    public Castle(String PLayerID, Team team, int nPlayer){
         if(nPlayer == 3) WRSize = 7;
         else WRSize = 9;
-        this.waitingRoom = new ArrayList<>(c);
+        this.waitingRoom = new ArrayList<>();
         this.diningRoom = new HashMap<>();
         this.cards = new ArrayList<>();
         this.towerColor = team;
@@ -21,33 +21,10 @@ public class Castle {
         for(int i=1; i<=10; i++) cards.add(new Card(i,(i+1)/2));
     }
 
-    public List<Color> getWaitingRoom() {
-        return new ArrayList<>(waitingRoom);
-    }
-
-    public Map<Color, Integer> getDiningRoom() {
-        return new HashMap<>(diningRoom);
-    }
-
-    public List<Card> getCards() {
-        return new ArrayList<>(cards);
-    }
-
-    public Card getLastCardPlayed() {
-        return lastCardPlayed;
-    }
-
-    public Team getTowerColor() {
-        return towerColor;
-    }
-
     public boolean addStudentWR(List<Color> c){
         try {
-            if(waitingRoom.size() + c.size() <= WRSize){
-                waitingRoom.addAll(c);
-                return true;
-            }
-            else return false;
+            waitingRoom.addAll(c);
+            return true;
         }catch(ArrayIndexOutOfBoundsException a){
             a.printStackTrace();
             return false;
@@ -96,9 +73,5 @@ public class Castle {
 
     public Team getTeam(){
         return towerColor;
-    }
-
-    public List<Card> remainingCards(){
-        return new ArrayList<>(cards);
     }
 }
