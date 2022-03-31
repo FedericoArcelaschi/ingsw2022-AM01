9f5d7e20979944e96c39be52cb42eddff95950e3
FedericoArcelaschi-Polimi <10654781@polimi.it>
2022-03-31 15:39:06 +0200

    Fixed small errors in Island, Castle and Cloud. Added multiple extraction in Bag. Fixed constructors in Board

--- src/main/java/it/polimi/ingsw/model/Castle.java
+++ src/main/java/it/polimi/ingsw/model/Castle.java
@@ -10,10 +10,10 @@ public class Castle {
     private final Team towerColor;
     private final int WRSize;
  
-    public Castle(String PLayerID, Team team, int nPlayer){
+    public Castle(String PLayerID, Team team, int nPlayer, List<Color> c){
         if(nPlayer == 3) WRSize = 7;
         else WRSize = 9;
-        this.waitingRoom = new ArrayList<>();
+        this.waitingRoom = new ArrayList<>(c);
         this.diningRoom = new HashMap<>();
         this.cards = new ArrayList<>();
         this.towerColor = team;
@@ -21,10 +21,33 @@ public class Castle {
         for(int i=1; i<=10; i++) cards.add(new Card(i,(i+1)/2));
     }
 
+    public List<Color> getWaitingRoom() {
+        return new ArrayList<>(waitingRoom);
+    }
+
+    public Map<Color, Integer> getDiningRoom() {
+        return new HashMap<>(diningRoom);
+    }
+
+    public List<Card> getCards() {
+        return new ArrayList<>(cards);
+    }
+
+    public Card getLastCardPlayed() {
+        return lastCardPlayed;
+    }
+
+    public Team getTowerColor() {
+        return towerColor;
+    }
+
     public boolean addStudentWR(List<Color> c){
         try {
-            waitingRoom.addAll(c);
-            return true;
+            if(waitingRoom.size() + c.size() <= WRSize){
+                waitingRoom.addAll(c);
+                return true;
+            }
+            else return false;
         }catch(ArrayIndexOutOfBoundsException a){
             a.printStackTrace();
             return false;
