f30605349f9f0dc4c23706b01e061d5bfb58bf71
Giovanni <10683631@polimi.it>
2022-04-03 13:52:12 +0200

    Second commit doesn't work
    WIP

--- src/main/java/it/polimi/ingsw/model/expert/InfluenceCharacter.java
+++ src/main/java/it/polimi/ingsw/model/expert/InfluenceCharacter.java
@@ -4,9 +4,10 @@ public class InfluenceCharacter extends Character{
     int cost;
     String explanation;
 
-    public InfluenceCharacter(int cost, InfluenceCharacterExecution execution) {
+    public InfluenceCharacter(int cost, ListCharacters char) {
+        this.cost = char.getcost();
         String explanation = "Gain the influence over a Professor even if in your castle \n you have as many students  "
-        super(cost, explanation);
+
 
     }
 
