37f48808b97dfe090ffe9c6fede767451977fb83
Giovanni <10683631@polimi.it>
2022-04-03 11:56:47 +0200

    First commit of the Expert Mode development
    + "rulebook" for CLI

---
+++ src/main/java/it/polimi/ingsw/model/expert/Character.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.model.expert;
+
+public abstract class Character {
+    int cost;
+    String explanation;
+
+    public Character(int cost, String explanation) {
+        this.cost = cost;
+        this.explanation = explanation;
+    }
+
+
+    public  String getExplanation(){
+        return explanation;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public abstract boolean applyEffect();
+}
---
+++ src/main/java/it/polimi/ingsw/model/expert/ListCharacters.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.expert;
+
+public enum ListCharacters { //TODO: complete the description
+    MONK, //Student //adds a student to an island
+    BANDIT, //Influence //Sposta l'influenza anche in caso di pareggio
+
+}
