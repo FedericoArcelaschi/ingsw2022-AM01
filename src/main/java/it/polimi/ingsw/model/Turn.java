c50f5982401256570caab786a4e49a37857dd3ee
Giovanni <10683631@polimi.it>
2022-04-05 19:33:30 +0200

    Updated Turn implementation and relative test

--- src/main/java/it/polimi/ingsw/model/Turn.java
+++ src/main/java/it/polimi/ingsw/model/Turn.java
@@ -4,49 +4,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Turn {
-    private final List<String> playerSittingOrder;
-    private List<String> playerPlanificationOrder;
-    private String playerTurn;
+    private final List<String> sittingOrder;
+    private List<String> actionOrder;
+    private String currentPlayerTurn;
 
     /**
      * For the first round the PlanificationTurn is the Sitting Order
-     * @param playerSittingOrder
+     * @param sittingOrder
      */
-    public Turn(List<String> playerSittingOrder) {
-        this.playerSittingOrder = new ArrayList<>(playerSittingOrder);
-        playerPlanificationOrder = new ArrayList<>(playerSittingOrder);
-        playerTurn = playerSittingOrder.get(0);
+    public Turn(List<String> sittingOrder) throws IllegalArgumentException{
+        if(sittingOrder.size() < 2 && sittingOrder.size() > 4)
+            throw new IllegalArgumentException();
+        this.sittingOrder = new ArrayList<>(sittingOrder);
+        actionOrder = new ArrayList<>(sittingOrder);
+        currentPlayerTurn = sittingOrder.get(0);
     }
 
     /**
      * @return the player who is playing
      */
     public String getTurn(){
-        return playerTurn;
+        return currentPlayerTurn;
     }
 
-    /**Changes the turn along the action turn order
+    /**Changes the turn along the new action turn order
      */
-    public void setTurnAction(List<String> newerTurns){
-        this.playerPlanificationOrder= newerTurns;
+    public void setTurnAction(List<String> newerTurns) throws IllegalArgumentException{
+        if(newerTurns.containsAll(actionOrder) && actionOrder.containsAll(newerTurns))
+            this.actionOrder = newerTurns;
+        else
+            throw new IllegalArgumentException();
+        currentPlayerTurn = actionOrder.get(0);
     }
 
     /** Sets the current turn to the player besides him.
      * @return playerTurn
      */
     public String nextTurnPlanification(){
-        int nextPlayerPosition = playerSittingOrder.indexOf(playerTurn) + 1;
-        if(nextPlayerPosition == playerSittingOrder.size())
-            return playerTurn = playerSittingOrder.get(0);
-        else return playerTurn = playerSittingOrder.get(nextPlayerPosition);
+        return currentPlayerTurn = next(sittingOrder,currentPlayerTurn);
     }
 
     /** Sets the current turn to the next player chose in the planification turn.
      * Returns also the first player for the Planification turn
+     * Doesn't need tp be called for the first player. It's already on currentPlayerTurn
      * @return playerTurn
      */
     public String nextTurnAction(){
-        int nextPlayerPosition = playerPlanificationOrder.indexOf(playerTurn) + 1;
-        return playerTurn = playerPlanificationOrder.get(nextPlayerPosition);
+        //actionOrder.get(0)
+        //actionOrder.remove(0)
+        int nextPlayerPosition = actionOrder.indexOf(currentPlayerTurn) + 1;
+        if(nextPlayerPosition == sittingOrder.size())
+            return null; //"It's Planification Time!!"
+        return currentPlayerTurn = actionOrder.get(nextPlayerPosition);
+    }
+
+    private String next(List<String> list, String element){
+        int dim = list.size();
+        int index = list.indexOf(element);
+        if(index == dim -1)
+            return list.get(0);
+        else return list.get(index +1);
     }
 }
--- src/test/java/it/polimi/ingsw/model/TurnTest.java
+++ src/test/java/it/polimi/ingsw/model/TurnTest.java
@@ -18,9 +18,24 @@ public class TurnTest extends TestCase {
     }
 
     public void testSetTurnAction() {
+        List<String> players = Arrays.asList("pippo","pluto","paperino");
+        Turn turn = new Turn(players);
+        players = Arrays.asList("pluto","paperino","pippo");
+        turn.setTurnAction(players);
+        for (String pl: players) {
+            assertEquals(turn.getTurn(), pl);
+            turn.nextTurnAction();
+        }
+
     }
 
     public void testNextTurnPlanification() {
+        List<String> players = Arrays.asList("pippo","pluto","paperino");
+        Turn turn = new Turn(players);
+        players = Arrays.asList("pluto","paperino","pippo");
+        turn.setTurnAction(players);
+        turn.nextTurnPlanification();
+        assertTrue(false);
     }
 
     public void testNextTurnAction() {
