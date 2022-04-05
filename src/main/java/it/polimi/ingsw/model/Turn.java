7840b97c7b027690f1616d2cd5adf64e6e2d32bc
Giovanni <10683631@polimi.it>
2022-04-05 22:00:14 +0200

    Turn - squashed commit of the following:
    commit c50f5982401256570caab786a4e49a37857dd3ee
    Author: Giovanni <10683631@polimi.it>
    Date:   Tue Apr 5 19:33:30 2022 +0200
    
        Updated Turn implementation and relative test
    
    commit 62b6f639d4a9c64aa00c8897c451e7f89e0d7539
    Author: FedericoArcelaschi-Polimi <10654781@polimi.it>
    Date:   Tue Apr 5 18:45:41 2022 +0200
    
        added NotYourTurnException, fixed nextTurn
    
    commit b42c7d6058b6bc972f5b840a7d9d8facb63ae5f3
    Author: Giovanni <10683631@polimi.it>
    Date:   Tue Apr 5 18:36:44 2022 +0200
    
        Updated Turn. Needs breakpoint while getting NextTurn

--- src/main/java/it/polimi/ingsw/model/Turn.java
+++ src/main/java/it/polimi/ingsw/model/Turn.java
@@ -4,37 +4,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Turn {
-    private final List<String> playerSittingOrder;
-    private final List<String> playerPianificationOrder;
-    private String playerTurn;
+    private final List<String> sittingOrder;
+    private List<String> actionOrder;
+    private String currentPlayerTurn;
 
-    public Turn(String playerTurn){
-        playerSittingOrder = new ArrayList<>();
-        playerPianificationOrder = new ArrayList<>();
-        this.playerTurn = playerTurn;
+    /**
+     * For the first round the PlanificationTurn is the Sitting Order
+     * @param sittingOrder
+     */
+    public Turn(List<String> sittingOrder) throws IllegalArgumentException{
+        if(sittingOrder.size() < 2 && sittingOrder.size() > 4)
+            throw new IllegalArgumentException();
+        this.sittingOrder = new ArrayList<>(sittingOrder);
+        actionOrder = new ArrayList<>(sittingOrder);
+        currentPlayerTurn = sittingOrder.get(0);
     }
-    /** Returns the turn of the current player.
-     *
-     * @return playerTurn
+
+    /**
+     * @return the player who is playing
      */
     public String getTurn(){
-        return playerTurn;
+        return currentPlayerTurn;
     }
 
-    /** Changes the turn.
-     *
+    /**Changes the turn along the new action turn order
      */
-    public void setTurn(List<String> newerTurns){
-        this.playerPianificationOrder.clear();
-        this.playerPianificationOrder.addAll(newerTurns);
+    public void setTurnAction(List<String> newerTurns) throws IllegalArgumentException{
+        if(newerTurns.containsAll(actionOrder) && actionOrder.containsAll(newerTurns))
+            this.actionOrder = newerTurns;
+        else
+            throw new IllegalArgumentException();
+        currentPlayerTurn = actionOrder.get(0);
     }
 
-    /** Sets the current turn to the next player.
-     *
+    /** Sets the current turn to the player besides him.
      * @return playerTurn
      */
-    public String nextTurn(){
-         this.playerTurn = playerTurn.replaceAll(playerTurn, playerPianificationOrder.get(playerPianificationOrder.indexOf(playerTurn)+1));
-         return playerTurn;
+    public String nextTurnPlanification(){
+        return currentPlayerTurn = next(sittingOrder,currentPlayerTurn);
+    }
+
+    /** Sets the current turn to the next player chose in the planification turn.
+     * Returns also the first player for the Planification turn
+     * Doesn't need tp be called for the first player. It's already on currentPlayerTurn
+     * @return playerTurn
+     */
+    public String nextTurnAction(){
+        //actionOrder.get(0)
+        //actionOrder.remove(0)
+        int nextPlayerPosition = actionOrder.indexOf(currentPlayerTurn) + 1;
+        if(nextPlayerPosition == sittingOrder.size())
+            return null; //"It's Planification Time!!"
+        return currentPlayerTurn = actionOrder.get(nextPlayerPosition);
+    }
+
+    private String next(List<String> list, String element){
+        int dim = list.size();
+        int index = list.indexOf(element);
+        if(index == dim -1)
+            return list.get(0);
+        else return list.get(index +1);
     }
 }
---
+++ src/test/java/it/polimi/ingsw/model/TurnTest.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.model;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TurnTest extends TestCase {
+
+    public void testGetTurn() {
+        List<String> players = Arrays.asList("pippo","pluto","paperino");
+        Turn turn = new Turn(players);
+        for (String pl: players) {
+            assertEquals(turn.getTurn(), pl);
+            turn.nextTurnPlanification();
+        }
+    }
+
+    public void testSetTurnAction() {
+        List<String> players = Arrays.asList("pippo","pluto","paperino");
+        Turn turn = new Turn(players);
+        players = Arrays.asList("pluto","paperino","pippo");
+        turn.setTurnAction(players);
+        for (String pl: players) {
+            assertEquals(turn.getTurn(), pl);
+            turn.nextTurnAction();
+        }
+
+    }
+
+    public void testNextTurnPlanification() {
+        List<String> players = Arrays.asList("pippo","pluto","paperino");
+        Turn turn = new Turn(players);
+        players = Arrays.asList("pluto","paperino","pippo");
+        turn.setTurnAction(players);
+        turn.nextTurnPlanification();
+        assertTrue(false);
+    }
+
+    public void testNextTurnAction() {
+    }
+}
