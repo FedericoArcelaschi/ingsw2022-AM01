f8e018f538b64202094ee7e908d73f1958d53784
Federico Arcelaschi <federico.arcelaschi@mail.polimi.it>
2022-04-12 09:47:06 +0200

    removed useless try-catch and sed methods

--- src/main/java/it/polimi/ingsw/model/Island.java
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -38,33 +38,16 @@ public class Island {
         this.ownership = ownership;
     }
 
-    public void setStudents(Map<Color, Integer> s){
-        this.students.clear();
-        for(Color c: s.keySet()) {
-            this.students.put(c, s.get(c));
-        }
-    }
-
     public boolean addStudent(Color c){
-        try {
-            students.put(c, students.get(c) + 1); //In the value of the color c, I'm putting the previous number of students + 1.
-            return true;
-        }catch(NullPointerException e){
-            e.printStackTrace();
-            return false;
-        }
+        students.put(c, students.get(c) + 1); //In the value of the color c, I'm putting the previous number of students + 1.
+        return true;
     }
 
     public boolean addStudent(Map<Color, Integer> s){
-        try {
-            for(Color c: s.keySet()) {
-                students.put(c, students.get(c) + s.get(c));
-            }
-            return true;
-        }catch(NullPointerException e){
-            e.printStackTrace();
-            return false;
+        for(Color c: s.keySet()) {
+            students.put(c, students.get(c) + s.get(c));
         }
+        return true;
     }
 
     private void towerInfluence(Map<Team, Integer> influence){
@@ -94,11 +77,4 @@ public class Island {
         return influence;
     }
 
-    @Override
-    public String toString() {
-        return "Island{" +
-                "students=" + students +
-                ", ownership=" + ownership +
-                '}';
-    }
 }
