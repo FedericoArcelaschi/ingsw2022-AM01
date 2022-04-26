0c07551f04a6b3e2ff20d6da55c7d3f0039093ed
LorenzoAicardi <lorenzo.aicardi@mail.polimi.it>
2022-04-26 15:24:03 +0200

    Minor fixes

--- src/main/java/it/polimi/ingsw/model/TurnPhases.java
+++ src/main/java/it/polimi/ingsw/model/TurnPhases.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw.model;
 
 public enum TurnPhases {
-    PLAYCARD,
-    MOVESTUDENTS,
-    MOVEMOTHERNATURE,
-    CHOOSECLOUD
+    PLANNING, //Phase in which players play cards
+    STUDENTS, //Phase in which players move the students
+    MOTHERNATURE, //Phase in which players move Mother Nature
+    CLOUD //Phase in which players choose the cloud.
 }
