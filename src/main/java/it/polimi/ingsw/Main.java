e0fb8c80d5f7b6842d30738402be00c17a12e22d
FedericoArcelaschi-Polimi <10654781@polimi.it>
2022-05-22 16:02:53 +0200

    Created a first preference panel

--- src/main/java/it/polimi/ingsw/Main.java
+++ src/main/java/it/polimi/ingsw/Main.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.gui.Gui;
+
 public class Main {
+    public static String[] args;
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Main.args = args;
+        Gui gui = new Gui();
+        gui.view();
     }
 }
