3bacd0f08ed57346982888ec5008df2513638a23
Giovanni <10683631@polimi.it>
2022-05-30 17:58:34 +0200

    Update LoginPaneController.java
    fixed missing attribute and added connection with controller

--- src/main/java/it/polimi/ingsw/userInterface/gui/controller/LoginPaneController.java
+++ src/main/java/it/polimi/ingsw/userInterface/gui/controller/LoginPaneController.java
@@ -4,42 +4,40 @@ import it.polimi.ingsw.client.ClientMain;
 import it.polimi.ingsw.userInterface.UserInterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 public class LoginPaneController {
     @FXML TextField usernameTextField, ipTextField, portTextField;
-    @FXML RadioButton player2RadioButton, player3RadioButton, player4RadioButton, baseModeRadioButton, expertModeRadioButton;
-    ToggleGroup nPlayer, mode;
-    UserInterface ui;
+    @FXML RadioButton player2RadioButton, player3RadioButton, player4RadioButton;
+    @FXML CheckBox expertModeButton;
+    ToggleGroup nPlayer;
+    UserInterface userInterface;
 
-    public void initialize(UserInterface ui){
+    public void initialize(UserInterface userInterface){
         nPlayer = new ToggleGroup();
-        mode = new ToggleGroup();
         player2RadioButton.setToggleGroup(nPlayer);
         player3RadioButton.setToggleGroup(nPlayer);
         player4RadioButton.setToggleGroup(nPlayer);
-        baseModeRadioButton.setToggleGroup(mode);
-        expertModeRadioButton.setToggleGroup(mode);
         player2RadioButton.setSelected(true);
-        baseModeRadioButton.setSelected(true);
-
-        this.ui = ui;
+        expertModeButton.setSelected(false);
+        this.userInterface = userInterface;
     }
 
     public void submitPreferences(ActionEvent actionEvent) {
         RadioButton selectedNPlayer = (RadioButton) nPlayer.getSelectedToggle();
-        RadioButton selectedMode = (RadioButton) mode.getSelectedToggle();
+        boolean expertMode = expertModeButton.selectedProperty().get();
 
         ClientMain clientMain = new ClientMain(
                 usernameTextField.getText(),
-                Integer.parseInt(selectedNPlayer.getText().substring(0,1)),
-                selectedMode.getText().equals("Expert Mode"),
+                Integer.parseInt(selectedNPlayer.getText().substring(0, 1)),
+                expertMode,
                 ipTextField.getText().equals("") ? ipTextField.getPromptText() : ipTextField.getText(),
                 Integer.parseInt(portTextField.getText().equals("") ? portTextField.getPromptText() : portTextField.getText())
         );
-        clientMain.connect(ui);
+        clientMain.connect(userInterface);
     }
 
     public void test(){
