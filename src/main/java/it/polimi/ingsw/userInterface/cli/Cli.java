47e52243f06e398d6f1798474c3aac305dca424c
Giovanni <10683631@polimi.it>
2022-05-31 08:13:15 +0200

    Cli() redesign

--- src/main/java/it/polimi/ingsw/userInterface/cli/Cli.java
+++ src/main/java/it/polimi/ingsw/userInterface/cli/Cli.java
@@ -2,38 +2,36 @@ package it.polimi.ingsw.userInterface.cli;
 
 import it.polimi.ingsw.client.ClientMain;
 import it.polimi.ingsw.controller.GameType;
+import it.polimi.ingsw.model.baseLogic.StudentColor;
 import it.polimi.ingsw.userInterface.UserInterface;
 import it.polimi.ingsw.communication.modelData.BoardData;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Cli implements UserInterface {
+public class Cli extends Application implements UserInterface {
     String username;
     int nPlayer;
-    boolean mode;
+    Boolean expertMode;
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public Cli() {
         //TODO: ask user preferences.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.println("Enter username:");
-            username = br.readLine();
-            System.out.println("Enter nPlayer:");
-            nPlayer = Integer.parseInt(br.readLine());
-            System.out.println("Expert mode?");
-            mode = Boolean.parseBoolean(br.readLine());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        getValidInput();
         ClientMain clientMain = new ClientMain(username,
                 nPlayer,
-                mode,
+                expertMode,
                 "127.0.0.1",
                 1234);
-        clientMain.connect(this);
+        Executors.newCachedThreadPool().submit(
+                () -> clientMain.connect(this));
+        //
         while(true) {
             try {
                 String input = br.readLine();
@@ -45,10 +43,18 @@ public class Cli implements UserInterface {
     }
 
     @Override
+    public void start(Stage stage) throws IllegalAccessException {
+       throw new IllegalAccessException("Method for GUI");
+    }
+
+    @Override
     public void draw(BoardData boardData) {
         System.out.println(boardData.toString());
     }
 
+    /**
+     * Method to handle the LobbyInfoMessages.
+     */
     @Override
     public void printWaitingRoom(List<String> connectedUser, GameType gameType) {
         StringBuilder sb = new StringBuilder();
@@ -57,6 +63,48 @@ public class Cli implements UserInterface {
         for (String user : connectedUser) {
             sb.append("\t").append(user).append("\n");
         }
-        sb.append("Game type: ").append(gameType).append("\n");
+        System.out.println(sb);
+    }
+
+    /**
+     * Before opening the connection with the server the "local" Cli requires to insert the preferences.
+     */
+    void getValidInput() {
+        String query;
+        try {
+            query = "Enter username:";
+            do{
+                System.out.println(query);
+                username = br.readLine();
+                query = "Enter a valid username:";
+            } while (username == null);
+
+            query = "Enter the number of players:";
+            do {
+                System.out.println(query);
+                try{
+                    nPlayer = Integer.parseInt(br.readLine());
+                } catch (NumberFormatException ignored){}
+                query = "Enter a valid number of players: (between 2 and 4)";
+            } while(nPlayer < 2 || nPlayer > 4);
+
+            query = "Expert mode? (Y/n)";
+            do {
+                System.out.println(query);
+                expertMode = getBoolean(br.readLine());
+            } while (expertMode == null);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private Boolean getBoolean(String s) {
+        if(s.equalsIgnoreCase("y"))
+            return true;
+        if(s.equalsIgnoreCase("n"))
+            return false;
+        return null;
     }
 }
