71344058c4a9a1568e683c5ee8f6e1d9d39c26f8
Giovanni <10683631@polimi.it>
2022-06-01 15:23:01 +0200

    Turn refactoring

--- src/main/java/it/polimi/ingsw/server/model/baseLogic/TurnPhase.java
+++ src/main/java/it/polimi/ingsw/server/model/baseLogic/TurnPhase.java
@@ -1,12 +1,17 @@
 package it.polimi.ingsw.server.model.baseLogic;
 
-public enum TurnPhase {
-    PLANNING(), //Phase in which players play cards
-    STUDENTS(), //Phase in which players move the students
-    MOTHERNATURE(), //Phase in which players move Mother Nature
-    CLOUD(); //Phase in which players choose the cloud.
+import org.jetbrains.annotations.Nullable;
 
-    TurnPhase(){
+public enum TurnPhase {
+    PLANNING,       //Phase in which players play cards
+    STUDENTS,       //Phase in which players move the students
+    MOTHERNATURE,   //Phase in which players move Mother Nature
+    CLOUD;          //Phase in which players choose the cloud.
+
+                    //Aside from the planning phase, players can play character cards at any time during their turn.
+    public @Nullable TurnPhase next() {
+        if(this != CLOUD)
+            return TurnPhase.values()[this.ordinal() + 1];
+        return null;
     }
-    //Aside from the planning phase, players can play character cards at any time during their turn.
 }
