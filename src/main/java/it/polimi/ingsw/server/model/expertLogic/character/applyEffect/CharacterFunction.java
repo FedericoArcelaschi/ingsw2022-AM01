40970c7803355a47fc280079c4bc15d8be42e822
Giovanni <10683631@polimi.it>
2022-07-01 01:40:28 +0200

    new assets

--- src/main/java/it/polimi/ingsw/server/model/expertLogic/character/applyEffect/CharacterFunction.java
+++ src/main/java/it/polimi/ingsw/server/model/expertLogic/character/applyEffect/CharacterFunction.java
@@ -118,7 +118,7 @@ public enum CharacterFunction {
                 islandToJoin = neightbouringIsland.subList(1, 3);
 
             if (islandToJoin == null)
-                return; //another escape if the conquered island won't join with the neighbours
+                return;
             //islands are joined
             islandList
                     .add(islandIndex,
