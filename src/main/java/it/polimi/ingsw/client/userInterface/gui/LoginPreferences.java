c19fff961858cafa3b3f7b38a2e968d3239e1bb4
FedericoArcelaschi-Polimi <10654781@polimi.it>
2022-07-01 21:46:33 +0200

    removed unused class

--- src/main/java/it/polimi/ingsw/client/userInterface/gui/LoginPreferences.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.client.userInterface.gui;
-
-import it.polimi.ingsw.communication.message.subclasses.Preferences;
-
-public record LoginPreferences(String IP, int port, Preferences preferences) {}
